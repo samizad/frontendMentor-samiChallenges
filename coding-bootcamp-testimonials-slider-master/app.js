1b216fa7615f3dd69b592b5c29325951023b457e
samizad <34879671+samizad@users.noreply.github.com>
2020-10-03 13:47:31 +0330

    Update app.js

--- coding-bootcamp-testimonials-slider-master/app.js
+++ coding-bootcamp-testimonials-slider-master/app.js
@@ -1,1 +1,5 @@
+const imageCon = document.getElementsByClassName("imageCon");
+const next = document.getElementsByClassName("nextArrow");
+const prev = ocument.getElementsByClassName("prevArrow");
 
+const images = ["image-tanya.jpg", "image-john.jpg"];
