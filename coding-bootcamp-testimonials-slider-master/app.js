5f10f22cc8b6158cfc3ab7da148a16da77beb91b
samizad <34879671+samizad@users.noreply.github.com>
2020-10-03 14:14:00 +0330

    Update app.js

--- coding-bootcamp-testimonials-slider-master/app.js
+++ coding-bootcamp-testimonials-slider-master/app.js
@@ -1,5 +1,22 @@
-const imageCon = document.getElementsByClassName("imageCon");
-const next = document.getElementsByClassName("nextArrow");
-const prev = ocument.getElementsByClassName("prevArrow");
-
-const images = ["image-tanya.jpg", "image-john.jpg"];
+var next = document.querySelectorAll(".next");
+var prev = document.querySelectorAll(".prev");
+var john = document.querySelector(".john");
+var tanya = document.querySelector(".tanya");
+console.log(john.getAttribute("display"));
+next.forEach(function (item) {
+  if (true) {
+    item.onclick = () => {
+      john.style.display = "block";
+      tanya.style.display = "none";
+    };
+  } else {
+    john.style.display = "none";
+    tanya.style.display = "block";
+  }
+});
+prev.forEach(function (item) {
+  item.onclick = () => {
+    john.style.display = "none";
+    tanya.style.display = "block";
+  };
+});
